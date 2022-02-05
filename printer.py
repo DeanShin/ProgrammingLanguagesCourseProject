c4f705510a3c09a5305faed56c1fa8fc8c1c5fdc
kunalbabbar1 <k_babbar@u.pacific.edu>
2022-02-04 17:40:51 -0800

    Added printer.py and cleaned code

--- printer.py
+++ printer.py
@@ -1,9 +1,12 @@
+from token import Token
+
 def printTokens(tokens):
     for tokenType, tokenValue in tokens:
         if tokenType is Token.NUMBER:
-            print(tokenValue+": NUMBER")
-        if tokenType is Token.IDENTIFIER:
-            print(tokenValue+": IDENTIFIER")
-        if tokenType is Token.SYMBOL:
-            print(tokenValue+": SYMBOL")
-    return
+            print(tokenValue+" : NUMBER")
+        elif tokenType is Token.IDENTIFIER:
+            print(tokenValue+" : IDENTIFIER")
+        elif tokenType is Token.SYMBOL:
+            print(tokenValue+" : SYMBOL")
+        elif tokenType is Token.ERROR):
+            print("ERROR READING \""+ tokenValue + "\"")
