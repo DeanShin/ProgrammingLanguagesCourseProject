baf16a33a892ece9400f3edf23f778e07c88aa4e
kunalbabbar1 <k_babbar@u.pacific.edu>
2022-03-09 18:45:09 -0800

    Added Parser Functions for implementation.

--- parser.py
+++ parser.py
@@ -1,5 +1,8 @@
 # Dean Shin and Kunal Babbar -- Phase 2.1
 
+from token import Token
+from ast import AbstractSyntaxTree
+
 class Parser():
     def __init__(self):
         self.tokenIdx = 0
@@ -7,9 +10,55 @@ class Parser():
         self.nextToken = None
     
     def parseTokens(self, tokens):
-        # Do something here
-        # Return abstract syntax tree
-        return
+        tree = self.consumeToken()
+        return tree
+
+    def parseExpr(self):
+        tree = self.parseTerm()
+        while self.nextToken[1] == "+":
+            self.consumeToken()
+            tree = AbstractSyntaxTree(self.nextToken, tree, self.parseTerm())
+        return tree
+
+    def parseTerm(self):
+        tree = self.parseFactor()
+        while self.nextToken[1] == "-":
+            self.consumeToken()
+            tree = AbstractSyntaxTree(self.nextToken, tree, self.parseFactor())
+        return tree
+
+    def parseFactor(self):
+        tree = self.parsePiece()
+        while self.nextToken[1] == "/":
+            self.consumeToken()
+            tree = AbstractSyntaxTree(self.nextToken, tree, self.parsePiece())
+        return tree
+
+    def parsePiece(self):
+        tree = self.parseElement()
+        while self.nextToken[1] == "*":
+            self.consumeToken()
+            tree = AbstractSyntaxTree(self.nextToken, tree, self.parseElement())
+        return tree
+
+    def parseElement(self):
+        if self.nextToken[1] == "(":
+            self.consumeToken()
+            tree = self.parseExpr()
+            if self.nextToken[1] != ")":
+                raise Exception("Expected ')', but got " + self.nextToken, self.nextToken)
+            self.consumeToken()
+            return tree
+        elif self.nextToken[0] == Token.NUMBER:
+            tree = AbstractSyntaxTree(self.nextToken)
+            self.consumeToken()
+            return tree
+        elif self.nextToken[0] == Token.IDENTIFIER:
+            tree = AbstractSyntaxTree(self.nextToken)
+            self.consumeToken()
+            return tree
+        else:
+            raise Exception("Expected '(', NUMBER, or IDENTIFIER, but got " + self.nextToken, self.nextToken)
 
     def consumeToken(self):
         self.nextToken = self.tokens[self.tokenIdx]
