cd6d090d8adbe3be423cfe5209011a9c7d29f13b
Dean Shin <deanshin@dhcp-10-15-152-26.wireless.pacific.edu>
2022-03-09 19:05:16 -0800

    Fix parser

--- parser.py
+++ parser.py
@@ -12,54 +12,61 @@ class Parser():
     def parseTokens(self) -> AbstractSyntaxTree:
         return self.parseExpr()
 
-    def parseExpr(self):
+    def parseExpr(self) -> AbstractSyntaxTree:
         tree = self.parseTerm()
-        while self.nextToken[1] == "+":
+        while self.nextToken and self.nextToken[1] == "+":
+            token = self.nextToken
             self.consumeToken()
-            tree = AbstractSyntaxTree(self.nextToken, tree, self.parseTerm())
+            tree = AbstractSyntaxTree(token, tree, self.parseTerm())
         return tree
 
-    def parseTerm(self):
+    def parseTerm(self) -> AbstractSyntaxTree:
         tree = self.parseFactor()
-        while self.nextToken[1] == "-":
+        while self.nextToken and self.nextToken[1] == "-":
+            token = self.nextToken
             self.consumeToken()
-            tree = AbstractSyntaxTree(self.nextToken, tree, self.parseFactor())
+            tree = AbstractSyntaxTree(token, tree, self.parseFactor())
         return tree
 
-    def parseFactor(self):
+    def parseFactor(self) -> AbstractSyntaxTree:
         tree = self.parsePiece()
-        while self.nextToken[1] == "/":
+        while self.nextToken and self.nextToken[1] == "/":
+            token = self.nextToken
             self.consumeToken()
-            tree = AbstractSyntaxTree(self.nextToken, tree, self.parsePiece())
+            tree = AbstractSyntaxTree(token, tree, self.parsePiece())
         return tree
 
-    def parsePiece(self):
+    def parsePiece(self) -> AbstractSyntaxTree:
         tree = self.parseElement()
-        while self.nextToken[1] == "*":
+        while self.nextToken and self.nextToken[1] == "*":
+            token = self.nextToken
             self.consumeToken()
-            tree = AbstractSyntaxTree(self.nextToken, tree, self.parseElement())
+            tree = AbstractSyntaxTree(token, tree, self.parseElement())
         return tree
 
-    def parseElement(self):
-        if self.nextToken[1] == "(":
+    def parseElement(self) -> AbstractSyntaxTree:
+        if self.nextToken and self.nextToken[1] == "(":
             self.consumeToken()
             tree = self.parseExpr()
             if self.nextToken[1] != ")":
                 raise Exception("Expected ')', but got " + self.nextToken, self.nextToken)
             self.consumeToken()
             return tree
-        elif self.nextToken[0] == Token.NUMBER:
+        elif self.nextToken and self.nextToken[0] == Token.NUMBER:
             tree = AbstractSyntaxTree(self.nextToken)
             self.consumeToken()
             return tree
-        elif self.nextToken[0] == Token.IDENTIFIER:
+        elif self.nextToken and self.nextToken[0] == Token.IDENTIFIER:
             tree = AbstractSyntaxTree(self.nextToken)
             self.consumeToken()
             return tree
         else:
             raise Exception("Expected '(', NUMBER, or IDENTIFIER, but got " + self.nextToken, self.nextToken)
 
-    def consumeToken(self):
+    def consumeToken(self) -> None:
         self.tokenIdx += 1
+        if self.tokenIdx >= len(self.tokens):
+            self.nextToken = None
+            return
         self.nextToken = self.tokens[self.tokenIdx]
         return
