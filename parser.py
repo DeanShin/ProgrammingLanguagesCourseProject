9c3d30be3f93d21e3c8047d507d7ecd9b3b9e356
Dean Shin <deanshin@dhcp-10-15-152-26.wireless.pacific.edu>
2022-03-09 17:33:48 -0800

    Outline

--- parser.py
+++ parser.py
@@ -1,1 +1,15 @@
 # Dean Shin and Kunal Babbar -- Phase 2.1
+
+nextToken = None
+tokenIdx = None
+
+def parseTokens(tokens):
+    # Do something here
+    
+    # Return abstract syntax tree
+    return
+
+
+def consumeToken():
+    # I will do something here
+    return
